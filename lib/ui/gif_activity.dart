c91f411da23bbb428029929ea366b76b7ce04768
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2020-08-03 21:32:41 -0300

    functionality of pressing the gif to share and added share button on the AppBar

--- lib/ui/gif_activity.dart
+++ lib/ui/gif_activity.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
 
@@ -12,6 +13,14 @@ class GifPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_gifData["title"]),
         backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: (){
+              Share.share(_gifData["images"]["original"]["url"]);
+            },
+          )
+        ],
       ),
       backgroundColor: Colors.black,
       body: Center(
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:coutinho_giphys/ui/gif_activity.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -123,6 +124,9 @@ class _HomePageState extends State<HomePage> {
                     MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))
                 );
               },
+              onLongPress: (){
+                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+              }
             );
           else
             return Container(
